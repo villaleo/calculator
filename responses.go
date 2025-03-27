d452d6c19681b25937a5716d52570d82e4801d36
Leo Villalobos <villaleobos@gmail.com>
2025-03-27 15:19:13 -0700

    nit: move common operations to common.go

--- responses.go
+++ responses.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
 )
@@ -43,31 +42,4 @@ func WriteError(w http.ResponseWriter, statusCode int, message string) {
 // server error is written instead.
 func WriteJson(w http.ResponseWriter, val any) {
 	WriteJsonWithStatus(w, http.StatusOK, val)
-}
-
-// MatchesMethod ensures that the request method matches the specified HTTP
-// method. A non-nil error means the method does not match the request method.
-func MatchesMethod(r *http.Request, method string) error {
-	if r.Method != method {
-		err := fmt.Errorf("method %s not allowed", r.Method)
-		return err
-	}
-
-	return nil
-}
-
-// DecodeRequestBody decodes the request body into val using json.Decode. val
-// should be passed by reference to ensure the decoded value is stored
-// correctly.
-func DecodeRequestBody(r *http.Request, val any) error {
-	defer r.Body.Close()
-
-	decoder := json.NewDecoder(r.Body)
-	decoder.DisallowUnknownFields()
-
-	if err := decoder.Decode(&val); err != nil {
-		return fmt.Errorf("received malformed request: %w", err)
-	}
-
-	return nil
 }
