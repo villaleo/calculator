f0c06abc7d58710551c996dd551eac99b1a15c76
Leo Villalobos <villaleobos@gmail.com>
2025-03-28 14:51:19 -0700

    nit: reformat main.go

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
-	mux := http.NewServeMux()
-
-	file, err := os.OpenFile("server.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0664)
+	logFile := "server.log"
+	file, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0664)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("failed to open file %q: %w", logFile, err))
 	}
 
+	mux := http.NewServeMux()
 	srv := NewServerAdapter(
 		WithHandler(mux),
 		WithAddr("localhost:3000"),
 		WithWriter(file),
 	)
 
-	// Register calculation endpoints
 	operations := []string{"add", "subtract", "multiply", "divide"}
 	for _, operation := range operations {
 		calculationHandler := http.HandlerFunc(srv.handleCalculation)
